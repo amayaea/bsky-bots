3b566e584f881b416e0f11de9bf3f4105db90ae7
amayaea <emilio.a.amaya@vanderbilt.edu>
2025-01-12 17:25:35 -0500

    add prettier

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # bsky-bots
+
 BlueSky bots created by https://bsky.app/profile/djmilli.bsky.social
 
 ## Follow the Bots
-- Is Virginia Mccaskey Still Alive: https://bsky.app/profile/isvmccaskeyalive.bsky.social
-- More to come... 
 
+- Is Virginia Mccaskey Still Alive: https://bsky.app/profile/isvmccaskeyalive.bsky.social
+- More to come...
+
+## Setup
+
+```
+yarn
+yarn husky init
+yarn build
+```
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,51 @@
-import { AtpAgent } from '@atproto/api';
-import * as dotenv from 'dotenv';
-import * as process from 'process';
-import wiki from 'wikipedia'
+import { AtpAgent } from "@atproto/api";
+import * as dotenv from "dotenv";
+import * as process from "process";
+import wiki from "wikipedia";
 
 dotenv.config();
 
 const agent = new AtpAgent({
-    service: 'https://bsky.social',
-})
- 
-const main = async () => {
-    const result = await getWikipedia()
-    const agent = await login()
-    post(agent, result)
-}
+  service: "https://bsky.social",
+});
 
-const getWikipedia = async () : Promise<boolean> => {
-    const page = await wiki.page('Virginia_Halas_McCaskey')
-    const info = await page.infobox()
-    return !info.hasOwnProperty('deathDate')
-}
+const main = async () => {
+  const result = await getWikipedia();
+  const agent = await login();
+  post(agent, result);
+};
+
+const getWikipedia = async (): Promise<boolean> => {
+  const page = await wiki.page("Virginia_Halas_McCaskey");
+  const info = await page.infobox();
+  return !info.hasOwnProperty("deathDate");
+};
 
 const login = async (): Promise<AtpAgent> => {
-    const username = process.env.ENV === 'dev' ? process.env.TEST_BOT_USERNAME : process.env.BLUESKY_USERNAME
-    const password = process.env.ENV === 'dev' ? process.env.TEST_BOT_PASSWORD : process.env.BLUESKY_PASSWORD
-    await agent.login({ identifier: username!, password: password!})
-    return agent
-}
+  const username =
+    process.env.ENV === "dev"
+      ? process.env.TEST_BOT_USERNAME
+      : process.env.BLUESKY_USERNAME;
+  const password =
+    process.env.ENV === "dev"
+      ? process.env.TEST_BOT_PASSWORD
+      : process.env.BLUESKY_PASSWORD;
+  await agent.login({ identifier: username!, password: password! });
+  return agent;
+};
 
 const post = async (agent: AtpAgent, result: boolean) => {
-    const post: string = result ? 'Yes, Virginia McCaskey is still alive.' : 'No, Virginia McCaskey is no longer alive.'
-    console.log(`Posting "${post}"`)
-    await agent.post({
-        text: post
-    });
-    console.log('Successfully Posted!')
-}
+  const post: string = result
+    ? "Yes, Virginia McCaskey is still alive."
+    : "No, Virginia McCaskey is no longer alive.";
+  console.log(`Posting "${post}"`);
+  await agent.post({
+    text: post,
+  });
+  console.log("Successfully Posted!");
+};
 
-main().catch(err => {
-    console.error(err);
-    process.exit(1);
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
