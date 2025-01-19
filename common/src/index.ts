5687cd80548083eea29b1eb8da49b0f8a3436f5c
amayaea <emilio.a.amaya@vanderbilt.edu>
2025-01-19 16:12:55 -0500

    fix Docker workspaces and upgrade yarn

--- common/src/index.ts
+++ common/src/index.ts
@@ -4,8 +4,11 @@ export const login = async (username: string, password: string): Promise<AtpAgen
   const agent = new AtpAgent({
     service: "https://bsky.social",
   });
-  const u = process.env.ENV === "dev" ? process.env.TEST_BOT_USERNAME : username;
-  const p = process.env.ENV === "dev" ? process.env.TEST_BOT_PASSWORD : password;
+  console.log(
+    `Logging in with ${process.env.NODE_ENV === "dev" ? "dev" : "production"} credentials`,
+  );
+  const u = process.env.NODE_ENV === "dev" ? process.env.TEST_BOT_USERNAME : username;
+  const p = process.env.NODE_ENV === "dev" ? process.env.TEST_BOT_PASSWORD : password;
   await agent.login({ identifier: u!, password: p! });
   return agent;
 };
