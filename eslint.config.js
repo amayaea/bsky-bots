f77ac3524a724e48d3bbe3458e9273eab79d920e
amayaea <emilio.a.amaya@vanderbilt.edu>
2026-01-04 17:38:23 -0500

    add eslint-plugin-unused-imports and ignore dist for lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,31 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import unusedImports from "eslint-plugin-unused-imports";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist/*", "package.json", "yarn.lock"] },
+  { ignores: ["**/dist/**", "package.json", "yarn.lock"] },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
