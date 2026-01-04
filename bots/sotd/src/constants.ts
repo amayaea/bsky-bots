77dd96e23a7cb4940f990847d7edb647ec65d751
amayaea <emilio.a.amaya@vanderbilt.edu>
2026-01-04 14:05:49 -0500

    improve hashtags

--- bots/sotd/src/constants.ts
+++ bots/sotd/src/constants.ts
@@ -3,4 +3,4 @@ export const PLAYLIST_ID = "7r5JIlUHFV7zmojVwcE4Xr";
 export const FIELDS_FILTER =
   "next,items(added_at,track(name,external_urls.spotify,artists(name,external_urls.spotify),album(name,release_date,images(url,height,width))))";
 export const TEST_BOT_HANDLE = "test-bot-account.bsky.social";
-export const HASHTAGS = ["#SongOfTheDay", "#SOTDBot"];
+export const HASHTAGS = ["#Music", "#MusicSky", "#NowPlaying", "#SongOfTheDay", "#SOTDBot"];
