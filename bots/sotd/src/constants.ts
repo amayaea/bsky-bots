1362a956d5d2a587f18e0088ada16b2313156920
amayaea <emilio.a.amaya@vanderbilt.edu>
2026-01-16 17:21:00 -0500

    fix hashtag and isSOTD post detection

--- bots/sotd/src/constants.ts
+++ bots/sotd/src/constants.ts
@@ -3,4 +3,5 @@ export const PLAYLIST_ID = "7r5JIlUHFV7zmojVwcE4Xr";
 export const FIELDS_FILTER =
   "next,items(added_at,track(name,external_urls.spotify,artists(name,external_urls.spotify),album(name,release_date,images(url,height,width))))";
 export const TEST_BOT_HANDLE = "test-bot-account.bsky.social";
+export const SOTD_PREFIX = "Today's Song of the Day is";
 export const HASHTAGS = ["#Music", "#MusicSky", "#NowPlaying", "#SongOfTheDay", "#SOTDBot"];
